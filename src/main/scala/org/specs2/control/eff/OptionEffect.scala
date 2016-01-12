5dbe3ad97e5bfaa9b7ada2beaf6dbd293ff16ff4
Eric Torreborre <etorreborre@yahoo.com>
2016-01-12 23:45:19 +1100

    wip

--- src/main/scala/org/specs2/control/eff/OptionEffect.scala
+++ src/main/scala/org/specs2/control/eff/OptionEffect.scala
@@ -38,5 +38,34 @@ object OptionEffect {
 
     interpret1[R, Option, A, Option[A]]((a: A) => Option(a))(recurse)(r)
   }
+
+  trait Removed[R <: Effects, U <: Effects, M[_]]
+
+
+  def runOption1[R <: Effects, U <: Effects, A](r: Eff[R, A])(implicit m: Member[Option, R], poly: Removed[R, U, Option]): Eff[U, Option[A]] =
+    r match {
+      case Pure(a) => EffMonad[U].pure(Some(a))
+      case Impure(u, c) =>
+        m.project(u) match {
+          case Some(Some(x)) =>
+            runOption1(c(x))
+          case Some(None) =>
+            EffMonad[U].pure(None)
+          case None =>
+            u match {
+              case UnionNext(n) =>
+                Impure(n.asInstanceOf[Union[R, u.X]], c).asInstanceOf[Eff[U, Option[A]]]
+              case UnionNow(mx) =>
+                Impure[U, u.X, Option[A]](u.asInstanceOf[Union[U, u.X]], Arrs.singleton(x => runOption1(c(x)))).asInstanceOf[Eff[U, Option[A]]]
+            }
+        }
+    }
+
+  implicit def removedFirst[R <: Effects, M[_]](implicit m: Member[M, M |: R]): Removed[M |: R, R, M] =
+    new Removed[M |: R, R, M] {}
+
+  implicit def removedNext[R <: Effects, U <: Effects, N[_], M[_]](implicit m: Member[M, R], removed: Removed[R, U, M]): Removed[N |: R, N |: U, M] =
+    new Removed[N |: R, N |: U, M] {}
+
 }
 
